ec93e4f1f8d6194bec94405dc62c0370f62f646d
hamidrprogrammer <hamidrprogrammer@gmail.com>
2025-02-24 18:44:45 +0300

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Download login script
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
+curl -s -o login.sh -L "https://raw.githubusercontent.com/hamidrprogrammer/startsh/refs/heads/main/mac/login.sh"
 
 # Disable spotlight
 sudo mdutil -i off -a
 
-# Create user with proper home directory
+# Create user
 sudo dscl . -create /Users/runneradmin
 sudo dscl . -create /Users/runneradmin UserShell /bin/bash
 sudo dscl . -create /Users/runneradmin RealName "Runner Admin"
@@ -20,11 +20,19 @@ sudo dscl . -append /Groups/admin GroupMembership runneradmin
 # Enable auto-login
 sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser -string "runneradmin"
 
-# Configure VNC
+# Configure Remote Management and VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
-  -activate -configure -access -on -restart -agent -privs -all
+  -activate -configure -access -on -privs -all -restart -agent
+
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
-  -configure -clientopts -setvnclegacy -vnclegacy yes -vncpassword "P@ssw0rd!"
+  -configure -clientopts -setvnclegacy -vnclegacy yes
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+  -configure -clientopts -setvncpw -vncpw "P@ssw0rd!"
+
+# Enable virtual display for headless environments
+sudo defaults write /Library/Preferences/com.apple.windowserver.plist DisplayResolutionEnabled -bool true
+sudo killall -HUP WindowServer
 
 # Install ngrok
 brew install --cask ngrok
