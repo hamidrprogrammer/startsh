38c37af21d51d6a7e3d68e986bcc31cdd91b8d9c
hamidrprogrammer <hamidrprogrammer@gmail.com>
2025-02-24 18:23:19 +0300

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -1,27 +1,39 @@
-#Downloads
+#!/bin/bash
+
+# Download login script
 curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
-#disable spotlight indexing
+
+# Disable spotlight
 sudo mdutil -i off -a
-#Create new account
+
+# Create user with proper home directory
 sudo dscl . -create /Users/runneradmin
 sudo dscl . -create /Users/runneradmin UserShell /bin/bash
-sudo dscl . -create /Users/runneradmin RealName Runner_Admin
+sudo dscl . -create /Users/runneradmin RealName "Runner Admin"
 sudo dscl . -create /Users/runneradmin UniqueID 1001
 sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
-sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/runneradmin
+sudo dscl . -passwd /Users/runneradmin "P@ssw0rd!"
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-#install ngrok
+
+# Enable auto-login
+sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser -string "runneradmin"
+
+# Configure VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+  -activate -configure -access -on -restart -agent -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+  -configure -clientopts -setvnclegacy -vnclegacy yes -vncpassword "P@ssw0rd!"
+
+# Install ngrok
 brew install --cask ngrok
-#configure ngrok and start it
-ngrok authtoken $1
-ngrok tcp 5900 --region=in &
+
+# Start ngrok
+ngrok authtoken "$1"
+nohup ngrok tcp 5900 --region=in --log=stdout > ngrok.log &
+sleep 10
+
+# Get Ngrok URL
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+echo "VNC URL: ${NGROK_URL/tcp:\/\//}"
